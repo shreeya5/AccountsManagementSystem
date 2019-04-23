c425810f3712bc0c0ba48553305693c6527ed601
Shreeya <shreeya5@connect.hku.hk>
2019-04-23 16:43:06 +0800

    add header file

--- main.cpp
+++ main.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
-#include <strings>
+#include <string>
+#include <vectors>
+#include <fstream>
+#include "AccountHolder.h"
 
 using namespace std;
 
-
-struct Category
+void Login_screen()
 {
-	string type[100];
-	double value[100];
-};
-
-struct User
-{
-	string username;
-	Categoy account, income, expense;
-	double budget;
-};
-
-void DisplayMainMenu()
-{
-	cout << "0: exit \n";
+	cout << "1. Do you want to create a new account?" << endl;
+	cout << "2. Do you want to log in an existing account?" << endl;
+	cout << "Please enter your prefered option : ";
 }
 
-
-//add
+void main()
+{
+	AccountHolder 
+	Login_Screen()
+	int userChoice;
+	string name;
+	AccountHolder user;
+	cin >> userChoice;
+	
+	do
+	{
+		switch(userChoice)
+		{
+			case 1:
+				cout << "Enter desired username :" ;
+				cin << name;
+				for(int i=0; i<AccountHolders.size(); i++)
+				{
+					if(name==AccountHolders[i].username)
+					{
+						cout << "Username already exists. Please enter another one."
+						cin << name;
+						i=0;
+					}
+				}
+				AccountHolders.push_back(name);
+		}
+	}
+}
